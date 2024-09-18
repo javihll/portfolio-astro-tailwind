712d2cd4c76591a26d9386a714078f5db56bcfa7
javihll <javihll@gmail.com>
2024-09-18 17:11:20 +0200

    Subida

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'https://rubygems.org'
 
 gem 'faraday'
 gem 'faraday-retry'
+gem "jekyll", "~> 4.2"
+
+group :jekyll_plugins do
+  gem "jekyll-timeago", "~> 0.13.1"
+end
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+
 export default defineConfig({
+  site: 'https://javihll.github.io',
+  base: '/portfolio-astro-tailwind>/',
   integrations: [tailwind(),react()]
 });
+
+
 
 
 
